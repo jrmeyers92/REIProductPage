325347c62d08436ff950e1d207c05666948e5354
Jake Meyers <jakejake92@yahoo.com>
2020-11-08 13:41:51 -0700

    added nav

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,6 +2,7 @@ import React from "react";
 import REILogo from "../REILogo/REILogo";
 import SearchBar from "../SearchBar/SearchBar";
 import "../Banner/Banner.css";
+import BannerNav from "../BannerNav/BannerNav";
 
 const Banner = () => {
 	return (
@@ -10,6 +11,7 @@ const Banner = () => {
 			<div className='banner-horz'>
 				<SearchBar />
 			</div>
+			<BannerNav />
 		</div>
 	);
 };
---
+++ src/components/BannerNav/BannerNav.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./BannerNav.css";
+
+const BannerNav = () => {
+	return (
+		<div className='bannernav'>
+			<ul className='bannernav__list'>
+				<li className='bannernav-item'>Gifts</li>
+				<li className='bannernav-item'>Camp & Hike</li>
+				<li className='bannernav-item'>Climb</li>
+				<li className='bannernav-item'>Cycle</li>
+				<li className='bannernav-item'>Paddle</li>
+				<li className='bannernav-item'>Run</li>
+				<li className='bannernav-item'>Fitness</li>
+				<li className='bannernav-item'>Snow</li>
+				<li className='bannernav-item'>Travel</li>
+				<li className='bannernav-item'>Men</li>
+				<li className='bannernav-item'>Women</li>
+				<li className='bannernav-item'>Kids</li>
+				<li className='bannernav-item'>Deals</li>
+				<li className='bannernav-item'>More</li>
+				<li className='bannernav-item'>REI OUTLET</li>
+			</ul>
+		</div>
+	);
+};
+
+export default BannerNav;
